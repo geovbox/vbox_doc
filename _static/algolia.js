8d89a5c1d8546d3f425d3f1a94a1e41ea3e4361c
geovbox <geovbox@163.com>
2021-04-12 00:42:19 +0800

    clean

--- _static/algolia.js
+++
@@ -1,9 +0,0 @@
-docsearch({
-  apiKey: 'deee6550242977b89b26faae69287043',
-  indexName: 'vbox',
-  inputSelector: '#rtd-search-form input[type=text]',
-  algoliaOptions: {
-    hitsPerPage: 6,
-  },
-  debug: false // Set debug to true if you want to inspect the dropdown
-});
